60626f974d861322df18488ed885c0bc22913af7
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-14 08:43:50 +0200

    fix: typo

--- src/features/books/booksStore.ts
+++ src/features/books/booksStore.ts
@@ -2,10 +2,11 @@ import { create } from "zustand";
 import { v4 as uuidv4 } from "uuid";
 import { devtools } from "zustand/middleware";
 import type {} from "@redux-devtools/extension"; // required for devtools typing
+import { createSelectors } from "@/hooks/createSelectors";
 
 export interface Book {
   id: string;
   title: string;
   author: string;
-  isbn: number;
+  isbn: string;
 }
