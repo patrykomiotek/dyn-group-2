edae2a3298e8c42b08ed5a786ccdc0adebe1cc1e
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-14 12:02:55 +0200

    feat: employees form with zod and react hook form

--- src/features/employees/AddEmployeeForm.tsx
+++ src/features/employees/AddEmployeeForm.tsx
@@ -1,33 +1,45 @@
+import { z } from "zod";
 import { Button, Input } from "@/shared/ui";
-import type { FormEventHandler } from "react";
 import { useEmployeeStore } from "./employeeStore";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const validationSchema = z.object({
+  name: z.string().min(1, { error: "Name is required" }), // error
+  role: z.string().min(1, { error: "Role is required" }), // z.enum(['CTO', 'CEO', ...])
+  salary: z.number().positive(),
+});
+
+type CreateEmployeeDto = z.infer<typeof validationSchema>;
 
 export const AddEmployeeForm = () => {
   const addEmployee = useEmployeeStore((store) => store.addEmployee);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateEmployeeDto>({
+    resolver: zodResolver(validationSchema),
+  });
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    // console.log(event.currentTarget);
-
-    const form = event.currentTarget;
-
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
-
-    console.log("Form Data:", data);
-
+  const handleEmployeeSubmit: SubmitHandler<CreateEmployeeDto> = (data) => {
     addEmployee({
-      name: data.name as string,
-      role: data.role as string,
-      salary: parseInt(data.salary as string, 10),
+      name: data.name,
+      role: data.role,
+      salary: data.salary,
     });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input label="Name" name="name" required />
-      <Input label="Role" name="role" required />
-      <Input label="Salary" name="salary" type="number" required />
+    <form onSubmit={handleSubmit(handleEmployeeSubmit)}>
+      <Input label="Name" {...register("name")} error={errors.name} />
+      <Input label="Role" {...register("role")} error={errors.role} />
+      <Input
+        label="Salary"
+        {...register("salary", { valueAsNumber: true })}
+        error={errors.salary}
+        type="number"
+      />
       <Button type="submit">Submit</Button>
     </form>
   );
